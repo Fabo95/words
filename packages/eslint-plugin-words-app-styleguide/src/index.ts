218b9205d816e0ba44856b26b290d017ba7b7744
Fabian <f-hinz@outlook.de>
2024-12-28 20:07:44 +0100

    removed styleguide

--- packages/eslint-plugin-words-app-styleguide/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { RuleKeysOrder } from "@styleguide/rules/keysOrder/ruleKeysOrder";
-import { RuleReactPropsOrder } from "@styleguide/rules/reactPropsOrder/ruleReactPropsOrder";
-
-module.exports = {
-  rules: {
-    "keys-order": RuleKeysOrder,
-    "react-props-order": RuleReactPropsOrder,
-  },
-};
