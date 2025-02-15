39290052b529c6a96586d2f0d66aebe1707ed683
Fabian <f-hinz@outlook.de>
2025-02-15 22:11:33 +0100

    formatted

--- service/src/app/[lang]/(loggedIn)/account/page.tsx
+++ service/src/app/[lang]/(loggedIn)/account/page.tsx
@@ -1,37 +1,36 @@
-import { AccountNameForm } from "@app/app/[lang]/(loggedIn)/account/_content/accountNameForm/accountNameForm";
-import { Box } from "@app/components/ui/box";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@app/components/ui/tabs";
-import { getTFunction } from "@app/utils/i18n/utils/i18nHelpers";
-import { Locale } from "@app/utils/locale/localeTypes";
-import { AccountPasswordForm } from "@app/app/[lang]/(loggedIn)/account/_content/accountPasswordForm/accountPasswordForm";
+import { AccountNameForm } from "@app/app/[lang]/(loggedIn)/account/_content/accountNameForm/accountNameForm"
+import { AccountPasswordForm } from "@app/app/[lang]/(loggedIn)/account/_content/accountPasswordForm/accountPasswordForm"
+import { Box } from "@app/components/ui/box"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@app/components/ui/tabs"
+import { getTFunction } from "@app/utils/i18n/utils/i18nHelpers"
+import { Locale } from "@app/utils/locale/localeTypes"
 
 export default async function ({ params }: { params: Promise<Record<"lang", Locale>> }) {
-    // --- STATE ---
+	// --- STATE ---
 
-    const { lang } = await params;
+	const { lang } = await params
 
-    const t = getTFunction(lang);
+	const t = getTFunction(lang)
 
-    // --- RENDER ---
+	// --- RENDER ---
 
-    return (
-        <Box className="justify-center pt-16 items-center">
-            <Tabs defaultValue="name" className="max-w-[400px] w-full">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="name">{t("pages.account.name.title")}</TabsTrigger>
+	return (
+		<Box className="justify-center pt-16 items-center">
+			<Tabs defaultValue="name" className="max-w-[400px] w-full">
+				<TabsList className="grid w-full grid-cols-2">
+					<TabsTrigger value="name">{t("pages.account.name.title")}</TabsTrigger>
 
+					<TabsTrigger value="password">{t("pages.account.password.title")}</TabsTrigger>
+				</TabsList>
 
-                    <TabsTrigger value="password">{t("pages.account.password.title")}</TabsTrigger>
-                </TabsList>
+				<TabsContent value="name">
+					<AccountNameForm />
+				</TabsContent>
 
-                <TabsContent value="name">
-                    <AccountNameForm />
-                </TabsContent>
-
-                <TabsContent value="password">
-                    <AccountPasswordForm />
-                </TabsContent>
-            </Tabs>
-        </Box>
-    );
+				<TabsContent value="password">
+					<AccountPasswordForm />
+				</TabsContent>
+			</Tabs>
+		</Box>
+	)
 }
