88c7cc021229a77590fde2aa9f03e0d86703384b
fabianhinz <f-hinz@outlook.de>
2026-02-19 15:33:48 +0100

    add github action to push docker file of web

--- services/web/next.config.ts
+++ services/web/next.config.ts
@@ -1,16 +1,12 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
-import path from 'path';
-import {fileURLToPath} from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
     output: 'standalone',
     turbopack: {
         resolveAlias: {
-            '@app': path.resolve(__dirname, 'src'),
+            '@app/*': './src/*',
         },
     },
 };
