464113a1c0fa2c944a92eb933788bd2bf4a25139
fabianhinz <f-hinz@outlook.de>
2026-02-19 15:31:39 +0100

    add github action to push docker file of web

--- services/web/next.config.ts
+++ services/web/next.config.ts
@@ -1,6 +1,9 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
