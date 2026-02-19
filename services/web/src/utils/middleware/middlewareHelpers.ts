b174e267f5a2d36210e71d95adad63b4bb1ef72e
fabianhinz <f-hinz@outlook.de>
2026-02-19 23:18:34 +0100

    add health route

--- services/web/src/utils/middleware/middlewareHelpers.ts
+++ services/web/src/utils/middleware/middlewareHelpers.ts
@@ -1,7 +1,6 @@
 import { LOGGED_IN_PATHNAME, LOGGED_OUT_PATHNAME, routing } from "@app/i18n/routing"
 import { URLPattern } from "next/server"
 import { Locale } from "@app/utils/locale/localeTypes"
-import { ENV } from "@app/utils/env/env"
 
 function buildPatterns(paths: typeof LOGGED_IN_PATHNAME | typeof LOGGED_OUT_PATHNAME) {
 	return paths.flatMap((path) => {
@@ -9,7 +8,7 @@ function buildPatterns(paths: typeof LOGGED_IN_PATHNAME | typeof LOGGED_OUT_PATH
 		return (Object.entries(routing.pathnames[path]) as [Locale, string][]).map(
 			([, localizedPath]) =>
 				// prefix with /:lang so we match `/de/anmeldung` *or* `/en/authentication`
-				new URLPattern({ pathname: `/:lang${localizedPath}`, baseURL: ENV.NEXT_PUBLIC_DEPLOYMENT_URL }),
+				new URLPattern({ pathname: `/:lang${localizedPath}` }),
 		)
 	})
 }
