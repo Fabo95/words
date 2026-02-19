3b33d5f02a7c3ef4333f2aae7544b5fd5df92451
fabianhinz <f-hinz@outlook.de>
2026-02-19 15:53:17 +0100

    add github action to push docker file of web

--- services/web/next.config.ts
+++ services/web/next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
     reactStrictMode: false,
     output: 'standalone',
+    bundler: 'webpack',
 };
 
 const withNextIntl = createNextIntlPlugin();
