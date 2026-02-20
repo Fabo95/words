918758f6bdd837a131b8abed4891b9852dd867af
fabianhinz <f-hinz@outlook.de>
2026-02-20 21:28:20 +0100

    fix pageContent alignment

--- services/web/src/app/[lang]/(loggedOut)/layout.tsx
+++ services/web/src/app/[lang]/(loggedOut)/layout.tsx
@@ -5,15 +5,11 @@ import { HydrationBoundary, dehydrate } from "@tanstack/react-query"
 import { PageContent } from "@app/components/ui/pageContent"
 
 export default async function Layout({ children }: { children: ReactNode }) {
-	// --- STATE ---
-
 	const queryClient = getQueryClient()
-
-	// --- RENDER ---
 
 	return (
 		<HydrationBoundary state={dehydrate(queryClient)}>
-			<PageContent className="min-h-dvh">{children}</PageContent>
+			<PageContent className="min-h-dvh ">{children}</PageContent>
 		</HydrationBoundary>
 	)
 }
--- services/web/src/components/ui/pageContent.tsx
+++ services/web/src/components/ui/pageContent.tsx
@@ -6,29 +6,21 @@ import { getMotion } from "@app/utils/motion/helpers"
 import { motion } from "motion/react"
 import { usePathname } from "@app/i18n/navigation"
 
-type PageContentProps = {
-	children: ReactNode
-	className?: string
-	fullHeight?: boolean
-}
+type PageContentProps = { children: ReactNode; className?: string }
 
-export const PageContent = ({ children, className, fullHeight = true }: PageContentProps) => {
+export const PageContent = ({ children, className }: PageContentProps) => {
 	const pathname = usePathname()
-
-	// --- RENDER ---
 
 	return (
 		<motion.div
 			key={pathname}
 			{...getMotion(0)}
 			className={cn(
-				"flex flex-col pt-6 pb-24 px-4 md:pt-16 md:pb-16 md:px-5 items-center",
-				fullHeight &&
-					"min-h-[calc(100dvh-var(--header-height)-var(--bottom-nav-height))] md:min-h-[calc(100dvh-var(--header-height-md))]",
+				"flex flex-col flex-1 overflow-y-auto items-center justify-center pt-6 pb-24 px-4 md:pt-16 md:pb-16 md:px-5",
 				className,
 			)}
 		>
-			<div className="my-auto w-full flex flex-col items-center">{children}</div>
+			{children}
 		</motion.div>
 	)
 }
